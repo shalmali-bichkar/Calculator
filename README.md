7dd2a3cfcf6e4ebc084bb6a5ca1b5889210337c4
Shalmali Bichkar <shalmalibichkar@gmail.com>
2024-09-05 06:05:44 +0530

    fully responsive calculator website

---
+++ README.md
@@ -0,0 +1,30 @@
+# Responsive Calculator Website 
+## 1. Introduction - 
+### I am currently a first year college student learning web development. I have made this website using HTML, CSS and JavaScript.
+## 2. Description - 
+### This is a fully responsive calculator website which includes the use of tools like flex and flexbox, media queries and toaster. The website is available in both DARK and LIGHT mode. Keyboard keys can can also be used to enter in values on the calculator
+
+    -- the modes can be switched by clicking the sun/moon symbols at the top right corner 
+    -- click on the equal to (=) button to get the answer (keyboard equivalent is enter key)
+    -- the AC button clears the calculator display area 
+    -- the back arrow (<=) removes a digit (keyboard equivalent is backspace key)
+
+
+## 3. Design : 
+### a) Computer
+#### i) Light-theme
+![alt text](image.png)
+#### ii) Dark-theme
+![alt text](image-1.png)
+
+## b) Tablet
+#### i) Vertical - 
+![alt text](image-5.png) 
+#### ii)Horizotal - 
+![alt text](image-6.png)
+
+## c) Phone:
+#### i) Vertical-
+![alt text](image-3.png) ![alt text](image-4.png)
+#### ii) Horizontal-
+![alt text](image-7.png) ![alt text](image-8.png)![alt text](image-9.png)
--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,59 +1,68 @@
 //const btns= document.getElementsByClassName("calc-btn") //returns Html element collection
-window.addEventListener('load',(e) => {
+window.addEventListener('load',(e) => { //to get the theme selected and stored on loading
     localStorage.getItem('theme')
     const body = document.querySelector('body')
     body.classList.add(localStorage.getItem('theme'))
     console.log(localStorage.getItem('theme'));
 })
 
-const btns = document.querySelectorAll(".calc-btn[btn-calc-val]") 
-const display_box = document.getElementById("display-box")
+const btns = document.querySelectorAll(".calc-btn[btn-calc-val]") //making an array of elements having class as calc-btn and attribute as  btn-calc-val
+const display_box = document.getElementById("display-box") //fetching element having id as display-box and storing it in display_box
 
 
-var calc_text = "";
+var calc_text = ""; //defining variables
 var calc_ans = 0.0;
 
 
-btns.forEach((b) => { 
+btns.forEach((b) => {  //everytime button clicked, content inside is executed
     b.addEventListener('click', (e) => {
-        display_box.innerHTML += b.innerText;
+        display_box.innerHTML += b.innerText; 
         typeof(display_box.innerHTML)
         calc_text += b.getAttribute("btn-calc-val") // to be calculated
-        if (!(isNaN(b.getAttribute("btn-calc-val"))) === true) 
+        if (!(isNaN(b.getAttribute("btn-calc-val"))) === true) //checks if the attribute ('btn-calc-val') is a number 
             displayDynamicAns()
 
-        if ((b.getAttribute("btn-calc-val")) === ")") 
+        if ((b.getAttribute("btn-calc-val")) === ")") //checks if attribute 9'btn-calc-val) = ( 
             displayDynamicAns()
     })
 })
 
-const dynamic_display_box = document.getElementById("dynamic-display-box")
-const displayDynamicAns = () => { 
+const dynamic_display_box = document.getElementById("dynamic-display-box") //
+
+const displayDynamicAns = () => { //creating function to check for errors in dynamic ans and calculating answer dynamically
     try{
         calc_ans = eval(calc_text);
         dynamic_display_box.innerHTML = calc_ans;
     }catch(error){
-        //console.log()
+        
     }
 }
 
-const displayAns = () => { 
+const displayAns = () => { //creating function to check for any errors in the calculation expression and calculating answer
     try{
         calc_ans = eval(calc_text)
         display_box.innerHTML = calc_ans;
-        calc_text = calc_ans
+        calc_text = "" + calc_ans //needs to be converted to string
     }catch(error){
-        window.alert("kya karra be")
+        showToast()
     }
-    
 }
 
 
-var equalBtn = document.getElementById("equal-btn")
-equalBtn.addEventListener("click", displayAns)
+var equalBtn = document.getElementById("equal-btn") //get element having id equal-btn (=)
+equalBtn.addEventListener("click", (e) => {
+    var display_text = ""
+    var eq_length  = ""
+    display_text = display_box.innerHTML
+    eq_length = display_text.length
 
-var allClearBtn = document.getElementById("all-clear-btn")
-allClearBtn.addEventListener('click', (e) => {
+    if (eq_length != 0){
+        displayAns()
+    }
+}) //displaying answer on clicking equal to btn
+
+var allClearBtn = document.getElementById("all-clear-btn") //get element having id all-clear btn(AC)
+allClearBtn.addEventListener('click', (e) => { //clearing everything on clicking all clear (AC) btn
     calc_text = ""
     display_box.innerHTML = ""
     dynamic_display_box.innerHTML = ""
@@ -61,16 +70,17 @@ allClearBtn.addEventListener('click', (e) => {
 
 
 
-const removeCharacter = () => {
+const removeCharacter = () => { //making function for removing a character from the equation ofr calculation
     var display_text = ""
     var eq_length  = ""
     display_text = display_box.innerHTML
     eq_length = display_text.length
     
-    calc_text = calc_text.slice(0,(eq_length-1))
+    calc_text = calc_text.slice(0,(eq_length-1)) 
     display_box.innerHTML = display_text.slice(0,(eq_length-1))
     dynamic_display_box.innerHTML = calc_text
-
+    if ((eq_length != 0)&&(eq_length != 1))
+        displayDynamicAns()
 }
 
 var btn_remove_character = document.getElementById("remove-digit-btn")
@@ -87,35 +97,47 @@ document.addEventListener('keydown', (e) => {
         calc_text += e.key
     } else if (e.keyCode === 187 || e.keyCode === 13) { // Equal to and Enter
         displayAns()   
-    } else if (e.keyCode === 8) { // Backspace 
+    } else if (e.keyCode === 8) { // [Backspace] 
         removeCharacter()
-    } else if(e.keyCode === 57 ||e.keyCode === 190){ // (  .
+    } else if(e.keyCode === 57 ||e.keyCode === 190){ // '('  '.' respectively
         display_box.innerHTML += e.key
         calc_text += e.key
-    }else if(e.keyCode === 48){ // )
+    }else if(e.keyCode === 48){ // ')' 
         display_box.innerHTML += e.key
         calc_text += e.key
         displayDynamicAns()
-    }else {
+    }else { //if clicked char is invalid 
         console.log("invalid char")
     }
     
 })
 
 
-const theme_btn = document.querySelector(".theme-switch-icon-container")
-theme_btn.addEventListener('click', (e) => {
+const theme_btn = document.querySelector(".theme-switch-icon-container")//selecting all elements having class theme-icon-switch-container
+theme_btn.addEventListener('click', (e) => { //switching theme on clicking 
     const body = document.querySelector("body");
     let class_to_remove = "", class_to_add = "" 
-    if (body.classList.contains("dark-theme")){
+    if (body.classList.contains("dark-theme")){ // if theme is light, remove class light-theme, add class dark-theme
         class_to_remove = "dark-theme";
         class_to_add = "light-theme";
     }
-    else {
-        class_to_remove = "light-theme";
+    else {//if theme is dark, remove class light-theme, add class dark-theme
+        class_to_remove = "light-theme"; 
         class_to_add = "dark-theme";
     }
     body.classList.remove(class_to_remove);
     body.classList.add(class_to_add);
-    window.localStorage.setItem('theme', class_to_add)
+    window.localStorage.setItem('theme', class_to_add)//store selected theme in local storage
 })
+
+const toast_box = document.getElementById('toast-box') //getting element having id as toast-box
+function showToast() { //showing toast
+    const toast = document.createElement('div')
+    toast.classList.add('toast')
+    toast.innerHTML = "Incorrect equation, Please recheck"
+    toast_box.appendChild(toast)
+
+    setTimeout(() =>{ //removing toast message
+        toast.remove()
+    },5000)
+}
